82adb3bf90f34ce44cd613c80342a24e705135f2
erikh360 <erikh360@gmail.com>
2026-02-05 11:50:55 +0200

    lint

--- src/celery_app.py
+++ src/celery_app.py
@@ -8,9 +8,7 @@ from celery import Celery
 celery_app = Celery("mc-intent-classifier")
 
 # RabbitMQ broker configuration
-broker_url = os.environ.get(
-    "CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672/"
-)
+broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672/")
 result_backend = os.environ.get("CELERY_RESULT_BACKEND", "rpc://")
 
 # Celery configuration
